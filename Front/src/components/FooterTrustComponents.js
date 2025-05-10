1521f39c99456fb647324c342582edb5807ccc88
paolo <paolo110101@yahoo.fr>
2025-05-10 16:36:54 +0200

    Ajout des images footer et des textes

--- Front/src/components/FooterTrustComponents.js
+++ Front/src/components/FooterTrustComponents.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import vietnam from '../assets/flags/vietnam.png';
+import japon from '../assets/flags/japon.png';
+import chine from '../assets/flags/chine.png';
+import coree from '../assets/flags/coree.png';
+import thailande from '../assets/flags/thailande.png';
+import cuisine from '../assets/cuisine.png';
+
+
 
 const TrustIcons = () => {
   return (
     <div className="bg-white py-8">
       <div className="container mx-auto">
-        <div className="grid grid-cols-2 md:grid-cols-5 gap-4 text-center px-16">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 text-center px-16">
           <div className="trust text-center">
             <figure className="flex justify-center">
               <img
-                src={require('../assets/reservePro.jpg')}
-                alt="Réservé aux pros"
+                src={require('../assets/cuisine.png')}
+                alt="Fait maison"
                 className="h-12 w-12 object-contain"
               />
             </figure>
@@ -20,46 +28,53 @@ const TrustIcons = () => {
             <figure className="flex justify-center">
               <img
                 src={require('../assets/paiementSecu.jpg')}
-                alt="Paiement pro sécurisé"
+                alt="Gouts asiatiques authentiques"
                 className="h-12 w-12 object-contain"
               />
             </figure>
-            <h3 className="font-bold mt-4">Paiement pro sécurisé</h3>
-            <p>Produit frais</p>
+            <h3 className="font-bold mt-4">Gouts asiatiques authentiques</h3>
+            <div className={'flex flex-row gap-2 justify-center'}>
+              <p>Cuisine</p>
+              <img src={vietnam} className={'h-6 w-6'}/>
+              <img src={japon} className={'h-6 w-6'}/>
+              <img src={thailande} className={'h-6 w-6'}/>
+              <img src={chine} className={'h-6 w-6'}/>
+              <img src={coree} className={'h-6 w-6'}/>
+            </div>
           </div>
           <div className="trust text-center">
             <figure className="flex justify-center">
               <img
-                src={require('../assets/retraitPerso.jpg')}
-                alt="Retrait personnalisé"
+                src={require('../assets/horloge.png')}
+                alt="Disponible à toute heure"
                 className="h-12 w-12 object-contain"
               />
             </figure>
-            <h3 className="font-bold mt-4">sdfsdfdsfsfds</h3>
-            <p>Gout vietnamiens authentique</p>
+            <h3 className="font-bold mt-4">Disponible à toute heure</h3>
+            <p>Lunch Box ou traiteur</p>
           </div>
           <div className="trust text-center">
             <figure className="flex justify-center">
               <img
-                src={require('../assets/arrivageRegu.jpg')}
-                alt="Arrivages réguliers"
+                src={require('../assets/vegetarien.png')}
+                alt="Végétarien friendly"
                 className="h-12 w-12 object-contain"
               />
             </figure>
-            <h3 className="font-bold mt-4">Arrivages réguliers</h3>
-            <p>Stock dispo à Lille</p>
+            <h3 className="font-bold mt-4">Végétarien friendly</h3>
+            <p>Toujours un choix végé disponible</p>
           </div>
-          <div className="trust text-center col-span-2 md:col-span-1">
+          {/*<div className="trust text-center col-span-2 md:col-span-1">
             <figure className="flex justify-center">
               <img
                 src={require('../assets/serviceClient.jpg')}
-                alt="Service client pro"
+                alt="Section commentaire"
                 className="h-12 w-12 object-contain"
               />
             </figure>
-            <h3 className="font-bold mt-4">Service client pro</h3>
-            <p>+33(0) 7 82 27 23 97</p>
-          </div>
+            <h3 className="font-bold mt-4">Section commentaire</h3>
+            <p>A l'écoute de vos envies</p>
+          </div>*/}
         </div>
       </div>
     </div>
